b7610598bf6287c8d5b7d09496b490696148a383
Luis_Alvarez <lolesuncrak@gmail.com>
2020-08-19 21:02:33 -0500

    Fixing Somethings

---
+++ docs/precache-manifest.6e623225cb6b616bf456af2d91f03627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd889afb0bb273cab31e4867186668b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc1ca87cba01e4a1eb46",
+    "url": "/static/css/main.0d2c9b19.chunk.css"
+  },
+  {
+    "revision": "ac6f1e0f4f2908a6bfe7",
+    "url": "/static/js/2.556c05b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.556c05b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1ca87cba01e4a1eb46",
+    "url": "/static/js/main.7247d556.chunk.js"
+  },
+  {
+    "revision": "142af1de3a19a148c46f",
+    "url": "/static/js/runtime-main.0c6910c8.js"
+  }
+]);
